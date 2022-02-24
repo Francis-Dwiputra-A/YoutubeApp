310402f0d5acb3d25e069d3e7886c337f2ce9f51
Francis-Dwiputra-A <francis.dwiputra17@gmail.com>
2022-02-24 14:43:53 +0700

    push

--- app/src/main/java/com/example/youtubeapp/about.java
+++ app/src/main/java/com/example/youtubeapp/about.java
@@ -6,15 +6,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class about extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class about extends AppCompatActivity {
+    EditText username, password;
+    Button submit;
+    String sendurl = "https://fjrmobileprog.000webhostapp.com/signUp.php";
+    RequestQueue req;
+    int success;
+    String TAG_Success = "success";
+    String TAG_Message = "message";
+    String tag_json_obj = "json_obj_req";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        submit = findViewById(R.id.submit);
+
+        req = Volley.newRequestQueue(getApplicationContext());
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                send_data();
+            }
+        });
         //initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
 
@@ -48,5 +85,42 @@ public class about extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    public void send_data(){
+        StringRequest request = new StringRequest(Request.Method.POST,
+                sendurl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jobj = new JSONObject(response);
+                            success = jobj.getInt(TAG_Success);
+                            if (success == 1) {
+                                Toast.makeText(about.this, jobj.getString(TAG_Message), Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(about.this, jobj.getString(TAG_Message), Toast.LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            Toast.makeText(about.this, "Error" + e, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(about.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("User_Name",username.getText().toString());
+                params.put("User_Password",password.getText().toString());  
+                return params;
+            }
+        };
+        request.setRetryPolicy(new DefaultRetryPolicy(10000,1,1.0f));
+        req.add(request);
     }
 }
